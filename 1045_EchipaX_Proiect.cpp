122a31e573745b18dccc79a7f1e2280afe839695
Matei Ovidiu <mateiovidiu10@gmail.com>
2023-12-18 13:05:25 +0200

    reordonare cod

--- 1045_EchipaX_Proiect.cpp
+++ 1045_EchipaX_Proiect.cpp
@@ -8,6 +8,12 @@ using namespace std;
 int main() {
 	string tipUser;
 
+	Produs** produseMagazin = new Produs * [5];
+	Magazin magazin("VOMMA", 0, produseMagazin);
+
+	Produs** cosClient = new Produs * [5];
+	Client client("Ovidiu", "0752", 18, 0, 0, cosClient);
+
 	// primim tipul de utilizator si nu permitem introducerea altor valori in afar de C si A
 	cout << "Cum vrei sa accesezi magazinul X? In calitate de Client (C) sau Administrator (A)?\n";
 	while (cin >> tipUser) {
@@ -16,18 +22,12 @@ int main() {
 		}
 	}
 	
-	Produs** produseMagazin = new Produs * [5];
-	Magazin magazin("VOMMA", 0, produseMagazin);
-
-	Produs** cosClient = new Produs * [5];
-	Client client("Ovidiu", "0752", 18, 0, 0, cosClient);
-	
 	if (tipUser == "C") {
 		cout << "Bine ai venit in magazinul " << magazin.getNume() << "\n";
 
 		while (1) {
 			string optiune;
-			cout << "Ce doresti sa faci in magazinul nostru azi?\n";
+			cout << "\nCe doresti sa faci in magazinul nostru azi?\n";
 			client.afiseazaMeniu();
 			while (cin >> optiune) {
 				if (optiune != "1" || optiune != "2" || optiune != "3" || optiune != "4" || optiune != "5" || optiune != "6" || optiune != "0")
